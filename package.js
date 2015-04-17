67f2d75fb946dc6cdadee39345820e43cdee0b46
Michael Mason <mjmasn@gmail.com>
2015-04-17 14:44:39 +0100

    Update README

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'mjmasn:maxmind',
-  version: '0.3.5',
+  version: '0.3.6',
   summary: 'MaxMind API package',
   git: 'https://github.com/mjmasn/maxmind',
   documentation: 'README.md'
